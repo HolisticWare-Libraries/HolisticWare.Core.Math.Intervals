1fc3e688671dfe3e3ee729c478b923c585e4d93f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-12-06 13:46:07 +0100

    Intervals source

---
+++ source/Core/Math/Intervals/HolisticWare.Core..Math.Intervals/Interval.Parse.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Core.Math.Intervals
+{
+    /// <summary>
+    /// Interval
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_(mathematics)"/>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_arithmetic"/>
+    /// http://mathworld.wolfram.com/Interval.html
+    public partial class Interval<T>
+    {
+        public static Interval<T> Parse(string interval_notation)
+        {
+            Interval<T> retval = default(Interval<T>);
+
+            string interval_notation_for_parsing;
+            interval_notation_for_parsing = interval_notation.TrimStart().TrimEnd();
+
+            int idx_lower_e = -1;
+            int idx_lower_i = -1;
+
+            idx_lower_e = interval_notation_for_parsing.IndexOf(Interval<T>.BoundLowerExcludedChar);
+            idx_lower_i = interval_notation_for_parsing.IndexOf(Interval<T>.BoundLowerIncludedChar);
+
+            if (idx_lower_e != -1 && idx_lower_i == -1)
+            {
+                retval.IsBoundLowerIncluded = false;
+                interval_notation_for_parsing = interval_notation_for_parsing.Remove(idx_lower_e, 1);
+            }
+
+            if (idx_lower_e == -1 && idx_lower_i != -1)
+            {
+                retval.IsBoundLowerIncluded = true;
+                interval_notation_for_parsing = interval_notation_for_parsing.Remove(idx_lower_i, 1);
+            }
+
+            int idx_upper_e = -1;
+            int idx_upper_i = -1;
+
+            idx_upper_e = interval_notation_for_parsing.IndexOf(Interval<T>.BoundUpperExcludedChar);
+            idx_upper_i = interval_notation_for_parsing.IndexOf(Interval<T>.BoundUpperIncludedChar);
+
+            if (idx_upper_e != -1 && idx_upper_i == -1)
+            {
+                retval.IsBoundUpperIncluded = false;
+                interval_notation_for_parsing = interval_notation_for_parsing.Remove(idx_upper_e, 1);
+            }
+
+            if (idx_upper_e == -1 && idx_upper_i != -1)
+            {
+                retval.IsBoundUpperIncluded = true;
+                interval_notation_for_parsing = interval_notation_for_parsing.Remove(idx_upper_i, 1);
+            }
+
+            string[] interval = interval_notation_for_parsing.Split(new char[]{','});
+            if (interval.Length > 2)
+            {
+                throw new InvalidOperationException("Interval parsing error");
+            }
+
+            try
+            {
+                retval.BoundLower = (T)Convert.ChangeType(interval[0], typeof(T));
+            }
+            catch(Exception exc)
+            {
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.AppendLine($"Interval<T>.Parse(string).BoundLower Error: ");
+                sb.AppendLine($"    Data = {interval[1]}");
+
+                throw new InvalidOperationException(sb.ToString(), exc);
+            }
+            try
+            {
+                retval.BoundUpper = (T)Convert.ChangeType(interval[1], typeof(T));
+            }
+            catch (Exception exc)
+            {
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.AppendLine($"Interval<T>.Parse(string).BoundUpper Error: ");
+                sb.AppendLine($"    Data = {interval[1]}");
+
+                throw new InvalidOperationException(sb.ToString(), exc);
+            }
+
+            return retval;    
+
+        }
+    }
+}
---
+++ source/Core/Math/Intervals/HolisticWare.Core..Math.Intervals/Interval.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Core.Math.Intervals
+{
+    /// <summary>
+    /// Interval
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_(mathematics)"/>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_arithmetic"/>
+    /// http://mathworld.wolfram.com/Interval.html
+    public partial class Interval<T>
+            /*
+            TODO:
+            Error CS0315: 
+            The type 'int' cannot be used as type parameter 'T' in the generic type or method 'Interval<T>'. 
+            There is no boxing conversion from 'int' to 'HolisticWare.Math.INumeric<int>'
+
+             where 
+                T : global::HolisticWare.Math.INumeric<T>
+            */
+        where T : IComparable, IComparable<T>
+    {
+        public T BoundUpper
+        {
+            get;
+            set;
+        }
+
+        public T BoundLower
+        {
+            get;
+            set;
+        }
+
+        public bool IsBoundUpperIncluded
+        {
+            get;
+            set;
+        }
+
+        public bool IsBoundLowerIncluded
+        {
+            get;
+            set;
+        }
+
+        public static char BoundLowerIncludedChar
+        {
+            get;
+            set;
+        }
+
+        public static char BoundLowerExcludedChar
+        {
+            get;
+            set;
+        }
+
+        public static char BoundUpperIncludedChar
+        {
+            get;
+            set;
+        }
+
+        public static char BoundUpperExcludedChar
+        {
+            get;
+            set;
+        }
+
+        //public static Interval<T> Parse(string interval_notation)
+        public Interval(string interval_notation)
+        {
+        }
+
+        public Interval
+                (
+                    T bound_lower, T bound_upper, 
+                    bool include_bound_lower = true, bool include_bound_upper = true
+                ) 
+        {
+            this.BoundLower = bound_lower;
+            this.BoundUpper = bound_upper;
+            this.IsBoundLowerIncluded = include_bound_lower;
+            this.IsBoundUpperIncluded = include_bound_upper;
+
+            return;
+        }
+
+        public Interval() 
+            : 
+            this (default(T), default(T), true, true)
+        {
+            return;
+        }
+
+        static Interval()
+        {
+            Interval<T>.BoundLowerIncludedChar = '[';
+            Interval<T>.BoundLowerExcludedChar = '(';
+            Interval<T>.BoundUpperIncludedChar = ']';
+            Interval<T>.BoundUpperExcludedChar = ')';
+
+            return;
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            if (this.IsBoundLowerIncluded == true)
+            {
+                sb.Append('[');   
+            }
+            else
+            {
+                sb.Append('(');   
+            }
+            sb.Append(this.BoundLower).Append(", ").Append(this.BoundUpper);
+            if (this.IsBoundUpperIncluded == true)
+            {
+                sb.Append(']');
+            }
+            else
+            {
+                sb.Append(')');
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Validate if Interval/Range is valid 
+        /// BoundLower (lower bound) is less or equal to BoundUpper(upper bound) 
+        /// (minimum is less or equal to maximum)
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if BoundLower is less or equal to BoundUpper
+        /// </returns>
+        public bool IsValid()
+        {
+            return this.BoundLower.CompareTo(this.BoundUpper) <= 0;
+        }
+
+
+        public bool Contains(T element)
+        {
+            bool lower_lt_element  = this.BoundLower.CompareTo(element) < 0;
+            bool lower_lte_element = this.BoundLower.CompareTo(element) <= 0;
+            bool upper_lt_element = this.BoundUpper.CompareTo(element) < 0;
+            bool upper_lte_element = this.BoundLower.CompareTo(element) <= 0;
+
+            bool contained =
+                lower_lt_element && lower_lte_element
+                &&
+                upper_lt_element && upper_lte_element
+                ;
+            
+            return contained;
+        }
+
+    }
+}
