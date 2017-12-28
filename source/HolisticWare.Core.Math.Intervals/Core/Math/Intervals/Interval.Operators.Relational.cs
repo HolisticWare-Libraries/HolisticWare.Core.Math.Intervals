c2d652c8e04d37e9e9bd763e111b9aaff00bd02e
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-12-28 22:10:20 +0100

    Operators Relational

---
+++ source/HolisticWare.Core.Math.Intervals/Core/Math/Intervals/Interval.Operators.Relational.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Core.Math.Intervals
+{
+    /// <summary>
+    /// Interval
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_(mathematics)"/>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_arithmetic"/>
+    /// http://mathworld.wolfram.com/Interval.html
+    public partial class Interval<T>
+    {
+        public static bool operator==(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+
+        public static bool operator !=(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+
+        public static bool operator <(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+
+        public static bool operator >(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+
+        public static bool operator <=(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+
+        public static bool operator >=(Interval<T> i1, Interval<T> i2)
+        {
+            bool retval = false;
+
+            return retval;
+        }
+    }
+}
