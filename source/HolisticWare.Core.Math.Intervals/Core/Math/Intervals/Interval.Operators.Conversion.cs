89fa1ef1152b17749b1efa087fca261f3dbb7178
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-12-28 22:09:41 +0100

    Operators Conversion

---
+++ source/HolisticWare.Core.Math.Intervals/Core/Math/Intervals/Interval.Operators.Conversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Core.Math.Intervals
+{
+    /// <summary>
+    /// Interval
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_(mathematics)"/>
+    /// <see cref="https://en.wikipedia.org/wiki/Interval_arithmetic"/>
+    /// http://mathworld.wolfram.com/Interval.html
+    public partial class Interval<T>
+    {
+        public static explicit operator Interval<T>(T t)  
+        {
+            Interval<T> i = new Interval<T>(t);  
+
+            return i;
+        }
+
+        public static explicit operator T(Interval<T> i)
+        {
+            T t = default(T);
+
+            return t;
+        }
+    }
+}
